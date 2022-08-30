ca0b9836170870141878b675854f0cd82309d76f
Jennifer Kelly <jenniferkelly@sbcglobal.net>
2022-08-30 12:35:03 -0700

    Added JS Confirm function to Ode

---
+++ JavaScript.file.Vincenzo.js
@@ -0,0 +1,34 @@
+
+function myFunction() {
+    alert("Hello From Mister");
+    let catResponse = prompt("Are You a Cat Lover?");
+    if (catResponse=="yes"){
+        document.write ("WooHoo! Welcome!")
+    }else if (catResponse=="no"){
+        document.write ("Bummer! Cat Lovers Are Cool")
+    }
+}
+
+function yourDecision() {//function definition
+var myCat = confirm("Do You Want to See Photos of my cat, Mister?");//taking confirm action from user if (black cat==true) //if press yes then value is true
+    {
+        document.write("Excellent! You're in the Right Place");//
+    }
+    do {
+	else
+} while (Cancel); //if press Cancel then value is false
+    {
+        document.write("Sorry to Hear That! Take Care!");
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
